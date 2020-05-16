78269a94c28e05671d094ca69df8849c3c1812f6
Z01iC <43041588+Z01iC@users.noreply.github.com>
2020-05-15 19:00:26 -0700

    updates

--- ADC.h
+++ ADC.h
@@ -2,12 +2,10 @@
 #define __ADC_H__
 
 /*These are the pins used for ADC in our project*/
-#define ADC1_SE18 0b10010
-#define ADC1_DM0 0b10011
-#define ADC1_DP0 0b00000
+#define PTB10 0b01110 /*Accelerometer X (ADC1_SE14)*/
+#define PTB11 0b01111  	/*Accelerometer Y (ADC1_SE15)*/ 
+#define PTC11 0b00111  /*Accelerometer Z (ADC1_SE7b)*/
 
 void ADC_Init(void); /*Initializes ADC Module on the board*/
-
 unsigned int ADC_read16b(char PIN); /*Read 16 bit ADC converted value*/
-
 #endif
