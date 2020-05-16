bd302f0929d2739f8a3af93663fda9d0c56e2265
hamiltonlee0 <52362102+hamiltonlee0@users.noreply.github.com>
2020-05-15 20:23:17 -0600

    adc fix

--- ADC.c
+++ ADC.c
@@ -5,10 +5,11 @@
   Function that initializes ADC Module on K64f
  *----------------------------------------------------------------------------*/
 void ADC_Init(void)	{
+	SIM->SCGC5    |= (1 <<  10);  /* Enable Clock to Port B & C */ 
 	SIM_SCGC3 |= SIM_SCGC3_ADC1_MASK; /*Enable the ADC1 Clock*/
-	ADC1_SC1A |= (1 << 5); /*Set Differential Mode of the ADC to off*/
 	ADC1_SC1A |= ADC_SC1_ADCH(0b11111); /*Module is Default set to OFF*/
-	ADC1_CFG1 |= ADC_CFG1_MODE(0b11); /*Set Mode to 16 bit conversion single ended (diff = 0)*/ 
+	ADC1_CFG1 |= ADC_CFG1_MODE(0b11); /*Set Mode to 16 bit conversion single ended (diff = 0)*/
+	SIM_SCGC6 |= SIM_SCGC6_ADC0_MASK; /*Enable the ADC1 Clock*/
 }
 
 /*----------------------------------------------------------------------------
@@ -19,4 +20,5 @@ unsigned int ADC_read16b(char PIN)	{
 	while(ADC1_SC2 & ADC_SC2_ADACT_MASK); /*ADACT set to 1 means conversion is active so wait until it's over*/
 	while(!(ADC1_SC1A & ADC_SC1_COCO_MASK)); /*Wait for Conversion to Finish*/
 	return ADC1_RA; /*Return the Value in the register*/
+
 }
